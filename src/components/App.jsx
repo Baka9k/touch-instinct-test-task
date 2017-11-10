d5f8293e1b853ae18cf65b395cfca3df3fffbd2c
Baka9k <neoplanspaceliner9@gmail.com>
2017-11-10 20:31:15 +0300

    Add React

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUploader from './FileUploader.jsx'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <div className="container">
+          <h1>Hello World</h1>
+          <FileUploader />
+        </div>
+      );
+  }
+}
---
+++ src/components/FileUploader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class FileUploader extends React.Component {
+  render() {
+    return (
+      <h4>Uploader</h4>
+    );
+  }
+}
