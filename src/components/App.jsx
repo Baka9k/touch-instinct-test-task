0d955ea16fec0cceb1c5a0ef1099896b1eb6cb89
Baka9k <neoplanspaceliner9@gmail.com>
2017-11-12 07:46:13 +0300

    Add mobile styles for App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,9 @@ export default class App extends React.Component {
   render () {
     return (
       <div id="app">
-        <div className="container">
-          <div id="content" className="centered centercontent">
-            <h1>Загрузите свой файл!</h1>
-            <FileUploader />
-          </div>
+        <div id="content" className="centered centercontent">
+          <h1>Загрузите свой файл!</h1>
+          <FileUploader />
         </div>
       </div>
     )
