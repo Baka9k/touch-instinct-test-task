7486a142ab30569d53b9f2d0d5c4a16736637a87
Baka9k <neoplanspaceliner9@gmail.com>
2017-11-12 07:19:30 +0300

    Add styles for body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import FileUploader from './FileUploader.jsx'
+import '../styles/App.scss'
 
 export default class App extends React.Component {
+
   render () {
     return (
-      <div className="container">
-        <h1>Hello World</h1>
-        <FileUploader />
+      <div id="app">
+        <div className="container">
+          <div id="content" className="centered centercontent">
+            <h1>Загрузите свой файл!</h1>
+            <FileUploader />
+          </div>
+        </div>
       </div>
     )
   }
+
 }
